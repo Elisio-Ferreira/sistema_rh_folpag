a15ab8da3eea570054800378ff5cf67eae42e873
Elisio-Ferreira <elisio.ferreira.dev@gmail.com>
2026-05-30 14:05:10 -0300

    atualizando a branch

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,24 @@
 package controller;
 
-public class Controller {
+import java.util.*;
 
+import model.FolhaPagamento;
+import service.FolhaPagamentoService;
+import service.RelatorioService;
+
+public class Controller {
+    private FolhaPagamentoService folhaPagamentoService = new FolhaPagamentoService();
+
+    public void cadastrarPagamento(FolhaPagamento pagamento) {
+        folhaPagamentoService.cadastrarPagamento(pagamento);
+    }
+    public List<FolhaPagamento> listarPagamentos() {
+        return folhaPagamentoService.listarPagamento();
+    }
+    public void atualizarPagamento(String nome, String novoNome) {
+        folhaPagamentoService.atualizarFolhaPagamento(nome, novoNome);
+    }
+    public void deletarPagamento(String nome) {
+        folhaPagamentoService.deletarFolhaPagamento(nome);
+    }
 }
--- src/service/FolhaPagamentoService.java
+++ src/service/FolhaPagamentoService.java
@@ -1,8 +1,14 @@
 package service;
 
+import model.FolhaPagamento;
 import model.Funcionario;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class FolhaPagamentoService {
+    List<FolhaPagamento> folhaPagamento = new ArrayList<>();
 
     public double calcularSalario(
             Funcionario funcionario) {
@@ -16,5 +22,35 @@ public class FolhaPagamentoService {
         return funcionario.getSalarioBase()
                 - desconto
                 + extra;
+    }
+
+    public void cadastrarPagamento(FolhaPagamento pagamento) {
+        folhaPagamento.add(pagamento);
+    }
+
+    public List<FolhaPagamento> listarPagamento() {
+        return folhaPagamento;
+    }
+
+    public void atualizarFolhaPagamento(String nome, String novoNome) {
+        for (FolhaPagamento pagamento : folhaPagamento) {
+            if (pagamento.getNome().equalsIgnoreCase(nome)) {
+                pagamento.setNome(novoNome);
+                System.out.println("Atualizado com sucesso!");
+                return;
+            }
+        }
+        System.out.println("Pagamento não foi encontrado.");
+    }
+
+    public void deletarFolhaPagamento(int id) {
+        for (int i = 0; i < folhaPagamento.size(); i++) {
+            if (folhaPagamento.get(i).getId()) {
+                folhaPagamento.remove(id);
+                System.out.println("Deletado com sucesso!");
+                return;
+            }
+            System.out.println("Pagamento não encontrado.");
+        }
     }
 }
